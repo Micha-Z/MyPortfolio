34db9c573b460f22d46140a947180d72269e6c0a
Michael Zadikowitsch <m.zadikowitsch@gmail.com>
2020-08-11 18:50:23 +0200

    clean up

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,12 +10,10 @@ window.onscroll = function () {
 
 function isAnyPartOfElementInViewport(el) {
   const rect = el.getBoundingClientRect();
-  // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
   const windowHeight =
     window.innerHeight || document.documentElement.clientHeight;
   const windowWidth = window.innerWidth || document.documentElement.clientWidth;
 
-  // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
   const vertInView = rect.top <= windowHeight && rect.top + rect.height >= 0;
   const horInView = rect.left <= windowWidth && rect.left + rect.width >= 0;
 
@@ -42,7 +40,6 @@ let contact = document.getElementById("contact");
 let micha = document.getElementById("micha");
 let my_resume = document.getElementById("my_resume");
 let submit = document.getElementById("submit");
-// console.log(my_resume);
 
 window.addEventListener(
   "scroll",
