48b193dd0a8bcae1adf7eda36abce11973adcec0
Michael Zadikowitsch <m.zadikowitsch@gmail.com>
2021-07-29 23:33:34 +0200

    clean up

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,27 +7,3 @@ window.onscroll = function () {
     navbar.classList.add("top");
   }
 };
-
-function isAnyPartOfElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  const windowHeight =
-    window.innerHeight || document.documentElement.clientHeight;
-  const windowWidth = window.innerWidth || document.documentElement.clientWidth;
-
-  const vertInView = rect.top <= windowHeight && rect.top + rect.height >= 0;
-  const horInView = rect.left <= windowWidth && rect.left + rect.width >= 0;
-
-  return vertInView && horInView;
-}
-
-var isInViewport = function (elem) {
-  var distance = elem.getBoundingClientRect();
-  return (
-    distance.top >= 0 &&
-    distance.left >= 0 &&
-    distance.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    distance.right <=
-      (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
--- dist/js/navbarHighlight.js
+++ dist/js/navbarHighlight.js
@@ -1,18 +1,31 @@
-let home = document.getElementById("home");
-let about = document.getElementById("about");
-let about_btn = document.getElementById("about_btn");
-let resume = document.getElementById("resume");
-let contact = document.getElementById("contact");
-let micha = document.getElementById("micha");
-let my_resume = document.getElementById("my_resume");
-let submit = document.getElementById("submit");
+const homeBtn = document.getElementById("home");
+const aboutBtn = document.getElementById("about");
+const resumeBtn = document.getElementById("resume");
+const contactBtn = document.getElementById("contact");
 
-viewList = [home, resume, contact, about];
+const getToKnowMeBtn = document.getElementById("gtnm-btn");
+const profilePicture = document.getElementById("profile-picture");
+const myResumeSection = document.getElementById("my-resume");
+const submitBtn = document.getElementById("submit");
 
-addCurrentTag = (element) => {
+const viewList = [homeBtn, resumeBtn, contactBtn, aboutBtn];
+
+const isInViewport = (elem) => {
+  let distance = elem.getBoundingClientRect();
+  return (
+    distance.top >= 0 &&
+    distance.left >= 0 &&
+    distance.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    distance.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const addCurrentTag = (element) => {
   element.classList.add("current");
   viewList
-    .filter((it) => element !== it)
+    .filter((viewListItem) => element !== viewListItem)
     .forEach((viewListItem) => {
       viewListItem.classList.remove("current");
     });
@@ -21,17 +34,17 @@ addCurrentTag = (element) => {
 window.addEventListener(
   "scroll",
   function () {
-    if (isInViewport(about_btn)) {
-      addCurrentTag(home);
+    if (isInViewport(getToKnowMeBtn)) {
+      addCurrentTag(homeBtn);
     }
-    if (isInViewport(micha)) {
-      addCurrentTag(about);
+    if (isInViewport(profilePicture)) {
+      addCurrentTag(aboutBtn);
     }
-    if (isInViewport(my_resume)) {
-      addCurrentTag(resume);
+    if (isInViewport(myResumeSection)) {
+      addCurrentTag(resumeBtn);
     }
-    if (isInViewport(submit)) {
-      addCurrentTag(contact);
+    if (isInViewport(submitBtn)) {
+      addCurrentTag(contactBtn);
     }
   },
   false
